786cbab0603efba242f9a5093a63f5223ef2162f
ghebby <gt.anatoli@yahoo.it>
2023-10-10 22:20:04 +0200

    Create index.php
    Just added the index.php

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+$greeting = "Hello, this is a test of a php app deployment on Azure Web App service"
+?>
+<!DOCTYPE html>
+<html>
+<head>
+  <title>AZ204 DemoApp Deploy</title>
+</head>
+<body>
+  <h1>Hello, World!</h1>
+  <p>
+    <?= $greeting ?>
+  </p>
+</body>
+</html>
